b8bf873513862bb4d74c1ac750179e1f0a401876
Caleb Wagner <calebwagner5@gmail.com>
2021-09-09 17:07:22 -0500

    created model for AppUser

---
+++ uteachilearnapi/models/app_user.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class AppUser(models.Model):
+    """Rare User Model"""
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.CharField(max_length=50)
+    image_url = models.ImageField() #install "pip install Pillow"
+    is_teacher = models.BooleanField()
