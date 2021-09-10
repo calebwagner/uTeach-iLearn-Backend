258446c89a019577883ccac9810698725912e50b
Caleb Wagner <calebwagner5@gmail.com>
2021-09-10 11:53:35 -0500

    added a cors setting

--- iteachulearn/settings.py
+++ iteachulearn/settings.py
@@ -69,6 +69,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+
 ]
 
 ROOT_URLCONF = 'iteachulearn.urls'
