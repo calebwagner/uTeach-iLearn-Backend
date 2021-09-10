507cad2a384b8ee0761f91f53ee2b31b8cd73d6d
Caleb Wagner <calebwagner5@gmail.com>
2021-09-10 06:13:52 -0500

    created model for posts

---
+++ uteachilearnapi/models/post.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    category = models.ForeignKey("Category", on_delete=models.CASCADE)
+    user = models.ForeignKey("AppUser", on_delete=models.CASCADE)
+    created_on = models.TimeField()
+    image = models.ImageField()
+    description = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.title
