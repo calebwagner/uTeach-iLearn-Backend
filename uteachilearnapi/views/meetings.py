19a3f769950c4b247bd01dc7c2cc7906836e6746
Caleb Wagner <calebwagner5@gmail.com>
2021-09-14 13:39:11 -0500

    created viewset for meetings

--- uteachilearnapi/views/meetings.py
+++ uteachilearnapi/views/meetings.py
@@ -1,16 +1,16 @@
 """View module for handling requests about events"""
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
+from django.db import connection
 from django.http import HttpResponseServerError
 from rest_framework import status
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import serializers
 from rest_framework.decorators import action
-from django.db.models import Count, fields
 from uteachilearnapi.models.app_user import AppUser
 from uteachilearnapi.models.meeting import Meeting
-
+from uteachilearnapi.models.connection import Connection
 
 
 class MeetingView(ViewSet):
@@ -23,7 +23,7 @@ class MeetingView(ViewSet):
         scheduler = AppUser.objects.get(user=request.auth.user)
 
         meeting = Meeting()
-        meeting.connection = request.data["title"]
+        meeting.connection = Connection.objects.get(pk=request.data['connection'])
         meeting.description = request.data["description"]
         meeting.created_on = request.data["created_on"]
         meeting.scheduled_date = request.data["scheduled_date"]
@@ -31,7 +31,7 @@ class MeetingView(ViewSet):
 
         try:
             meeting.save()
-            serializer = EventSerializer(meeting, context={'request': request})
+            serializer = MeetingSerializer(meeting, context={'request': request})
             return Response(serializer.data)
         except ValidationError as ex:
             return Response({"reason": ex.message}, status=status.HTTP_400_BAD_REQUEST)
@@ -42,31 +42,12 @@ class MeetingView(ViewSet):
             Response -- JSON serialized game instance
         """
         try:
-            event = Event.objects.get(pk=pk)
-            serializer = EventSerializer(event, context={'request': request})
+            meeting = Meeting.objects.get(pk=pk)
+            serializer = MeetingSerializer(meeting, context={'request': request})
             return Response(serializer.data)
         except Exception as ex:
             return HttpResponseServerError(ex)
 
-    def update(self, request, pk=None):
-        """Handle PUT requests for an event
-        Returns:
-            Response -- Empty body with 204 status code
-        """
-        gamer = Gamer.objects.get(user=request.auth.user)
-
-        event = Event()
-        event.title = request.data["title"]
-        event.date = request.data["date"]
-        event.time = request.data["time"]
-        event.description = request.data["description"]
-        event.host = gamer
-
-        game = Game.objects.get(pk=request.data["game"])
-        event.game = game
-        event.save()
-
-        return Response({}, status=status.HTTP_204_NO_CONTENT)
 
     def destroy(self, request, pk=None):
         """Handle DELETE requests for a single game
@@ -74,68 +55,59 @@ class MeetingView(ViewSet):
             Response -- 200, 404, or 500 status code
         """
         try:
-            event = Event.objects.get(pk=pk)
-            event.delete()
+            meeting = Meeting.objects.get(pk=pk)
+            meeting.delete()
 
             return Response({}, status=status.HTTP_204_NO_CONTENT)
 
-        except Event.DoesNotExist as ex:
+        except Meeting.DoesNotExist as ex:
             return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
 
         except Exception as ex:
             return Response({'message': ex.args[0]}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
     def list(self, request):
-        """Handle GET requests to events resource
+        """Handle GET requests to posts resource
 
         Returns:
-            Response -- JSON serialized list of events
+            Response -- JSON serialized list of posts
         """
-        # Get the current authenticated user
-        gamer = Gamer.objects.get(user=request.auth.user)
-        events = Event.objects.all()
+        # Get all game records from the database
+        meetings = Meeting.objects.filter(scheduler_id=request.auth.user.id)
 
-        # Set the `joined` property on every event
-        for event in events:
-            # Check to see if the gamer is in the attendees list on the event
-            event.joined = gamer in event.attendees.all()
-
-        # Support filtering events by game
-        game = self.request.query_params.get('gameId', None)
-        if game is not None:
-            events = events.filter(game__id=type)
-
-        serializer = EventSerializer(
-            events, many=True, context={'request': request})
+        serializer = MeetingSerializer(
+            meetings, many=True, context={'request': request})
         return Response(serializer.data)
 
 
-
-class EventUserSerializer(serializers.ModelSerializer):
-    """JSON serializer for event organizer's related Django user"""
+class UserSerializer(serializers.ModelSerializer):
+    """JSON serializer for users name"""
     class Meta:
         model = User
-        fields = ['first_name', 'last_name', 'email']
+        fields = ['id','first_name', 'last_name', 'email']
 
-class EventGamerSerializer(serializers.ModelSerializer):
-    """JSON serializer for event organizer"""
-    user = EventUserSerializer(many=False)
-
-    class Meta:
-        model = Gamer
-        fields = ['user']
-
-class GameSerializer(serializers.ModelSerializer):
-    """JSON serializer for games"""
-    class Meta:
-        model = Game
-        fields = ('id', 'name', 'description', 'maker', 'number_of_players', 'gamer','game_type', 'event_count')
-
-class EventSerializer(serializers.ModelSerializer):
-    """JSON serializer for events"""
-    host = EventGamerSerializer(many=False)
-    game = GameSerializer(many=False)
+class AppUserSerializer(serializers.ModelSerializer):
+    """JSON serializer for users"""
+    user = UserSerializer(many=False)
 
     class Meta:
-        model = Event
-        fields = ('id', 'title', 'date', 'time', 'description', 'host', 'game', 'joined')
+        model = AppUser
+        fields = ['id','user']
+
+class ConnectionSerializer(serializers.ModelSerializer):
+    """JSON serializer for users"""
+    user = UserSerializer(many=False)
+
+    class Meta:
+        model = Connection
+        fields = ['id','user']
+
+class MeetingSerializer(serializers.ModelSerializer):
+    """JSON serializer for posts"""
+    scheduler = AppUserSerializer(many=False)
+    connection = ConnectionSerializer(many=False)
+
+    class Meta:
+        model = Meeting
+        fields = ('id', 'scheduler', 'connection', 'description', 'created_on','scheduled_date')
+        depth = 1
