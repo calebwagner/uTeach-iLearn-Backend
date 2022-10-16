19bbe483aae3f13172c2ad4f878336ff1432d3bd
Caleb <calebwagner5@gmail.com>
2022-10-16 07:51:00 -0500

    transition to mysql

--- uteachilearnapi/models/app_user.py
+++ uteachilearnapi/models/app_user.py
@@ -3,9 +3,13 @@ from django.contrib.auth.models import User
 
 
 class AppUser(models.Model):
-    user = models.OneToOneField(User ,on_delete=models.CASCADE)
-    bio = models.CharField(max_length=50)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.CharField(max_length=200)
     image_url = models.ImageField(
-        upload_to='media/images', height_field=None,
-        width_field=None, max_length=None, null=True)
+        upload_to="media/images",
+        height_field=None,
+        width_field=None,
+        max_length=None,
+        null=True,
+    )
     # is_teacher = models.BooleanField()
